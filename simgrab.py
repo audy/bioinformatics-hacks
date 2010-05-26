a15fce14ec44757d6cce5ce5f7d4792d299a9623
Austin G. Davis-Richardson <harekrishna@gmail.com>
2010-05-26 15:36:56 -0400

    added simgrab

---
+++ simgrab.py
@@ -0,0 +1,32 @@
+# simgrab.py
+
+# Austin G. Davis-Richardson
+# Two fasta files
+# Scripts takes header from one, gets corresponding sequence from two
+# Prints out two
+
+import sys
+
+
+# first argument is query
+# second argument is database
+
+query = []
+
+with open(sys.argv[1]) as handle:
+    if line[0] == '>':
+        query.append(line.strip())
+
+query = set(query)
+
+print_seq = False
+
+with open(sys.argv[2]) as handle:
+    if line[0] == '>':
+        if line.strip() in query:
+            print_seq = True
+            print line
+        else:
+            print_seq = False
+    elif print_seq:
+        print line
