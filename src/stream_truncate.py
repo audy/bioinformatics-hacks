7011964e909de671f5bfdf23be56dc75f3d894e4
Austin G. Davis-Richardson <harekrishna@gmail.com>
2010-09-09 16:18:34 -0400

    fixed that pesky first and last fasta iterating bug

--- src/stream_truncate.py
+++ src/stream_truncate.py
@@ -6,7 +6,7 @@
 
 import sys
 
-header = ''
+header = False
 sequence = []
 
 beg = int(sys.argv[1])
@@ -14,8 +14,10 @@ end = int(sys.argv[2])
 
 for line in sys.stdin:
     if line[0] == '>':
-        print header, ''.join(sequence)[beg:end]
+        if header: print header, ''.join(sequence)[beg:end]
         header = line
         sequence = []
     else:
         sequence.append(line.strip())
+
+print header, ''.join(sequence)[beg:end]
