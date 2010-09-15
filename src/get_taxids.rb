13b8821c61f1193d9f059314f2227cdc56d725ed
Austin G. Davis-Richardson <harekrishna@gmail.com>
2010-09-15 12:33:02 -0400

    Now queries 30 at a time using NCBI-style FASTA headers AND Retries up to 5 times if there's a SocketError WOOHOOO! I <3 Ruby

--- src/get_taxids.rb
+++ src/get_taxids.rb
@@ -14,20 +14,37 @@ records = []
 
 file.each do |line|
   if line.include? '>'
-    id = line.split(' | ')[-3]
+    id = line.split('|')[3]
     records << id
   end
 end
 
-records.each do |record|
-  rec = ncbi.efetch(record, {"db"=>"nucleotide", "rettype"=>"gb", "retmode"=>"xml"})
+records.uniq!
+
+while records.length > 0
+  query = records.pop(30)
+  retries = 5
+  begin
+    rec = ncbi.efetch(query.join(','), {"db"=>"nucleotide", "rettype"=>"gb", "retmode"=>"xml"})
+  rescue SocketError
+    if (retries -= 1) > 0
+      retry
+    else
+      next
+    end
+  end
   doc = Hpricot::XML(rec)
-  doc.search('GBQualifier_value').each do |h|
-    if h.to_s.include? 'taxon:'
-      a = h.to_s.split(':')
-      a = a[1].split('</')
-      id = a[0]
-      puts '%s => %s' % [record, id]
+  numb = query.pop
+  
+  doc.search('GBSeq').each do |result|
+    gi = result.search('GBSeq_accession-version>')
+    result.search('GBQualifier_value').each do |h|
+      if h.to_s.include? 'taxon:'
+        a = h.to_s.split(':')
+        a = a[1].split('</')
+        id = a[0]
+        puts '%s => %s' % [gi, id]
+      end
     end
   end
 end
