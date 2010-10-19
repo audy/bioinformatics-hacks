b36e34f6af60e5c3375584bd3ab0bfa2efaeb669
Austin G. Davis-Richardson <harekrishna@gmail.com>
2010-10-19 19:35:38 -0400

    this doesnt work. I hate perl :( I just ended up making it in Python

--- src/taxsearch.pl
+++ src/taxsearch.pl
@@ -10,8 +10,8 @@ Written by:
     University of Florida
 
 Usage:
-    -t taxa keyword
-    -c cutoff value (decimal)
+    -t keywords file
+    -c cutoff value (percentage)
     -d megablast query (.fasta)
     -m megablast results (.txt)
     -o name of output file
@@ -19,44 +19,61 @@ Usage:
 
 use strict;
 use Getopt::Std;
+use IO::File;
 
+# GET OPTIONS
 my %args;
-my %heads = ();
-my $match = 0;
+getopts('t:c:d:m:o:',\%args);
+if (keys %args == 0) { print $help; exit; }
+foreach my $key (keys %args) { unless ($args{$key}) { print $help; exit; } }
 
-getopts('t:c:d:m:o:', \%args);
+# OPEN FILES
+open(FASTA, $args{'d'}) or die "Can't open $args{'d'}!";
+open(MEGABLAST, $args{'m'}) or die "Can't open $args{'m'}!";
+open(KEYWORDS, $args{'t'}) or die "Can't open $args{'t'}!";
 
-foreach my $key (keys %args) {unless ($args{$key}) { print $help; exit; } }
+# READ KEYWORDS; MAKE OUTPUT HANDLES
+my %keywords;
+while (my $line = <KEYWORDS>) {
+  chomp $line;
+  $line =~ s/\///;
+  my $handle = IO::File->new;
+  $handle->open(">$line.fas") or die "Can't open $line!";
+  $keywords{"$line"} = $handle;
+}
 
-open(RESULTS, $args{'d'}) or die "can't open $args{'d'}";
-open(QUERY, $args{'m'}) or die "can't open $args{'m'}";
-
-open OUT, ">$args{o}" or die $!;
-
-while (my $line = <QUERY>) {
-  if (eval($line =~ /$args{t}/)) {
-    my @columns = split(/\t/, $line);
-    if ($columns[2] >= $args{c}) {
-      $heads{"$columns[0]"} =  "$columns[1]";
+# MEGABLAST LOOP
+my %good_records;
+my @keys = keys %keywords;
+while (my $line = <MEGABLAST>) {
+  my @columns = split(/\t/, $line);
+  foreach my $key (@keys) {
+    if (("$columns[1]" =~ /\Q$key\E/) and ($columns[2] >= $args{c})) {
+      $good_records{"$columns[0]"} = "$columns[1]";
     }
   }
 }
 
-while (my $line = <RESULTS>) {
+# FASTA LOOP; PRINT TABLES
+my $match = 0;
+my $handle;
+while (my $line = <FASTA>) {
   if ($line =~ /^>/) {
-    $match = 0;   
-    chomp $line;
+    $match = 0;
     chop $line;
+    chomp $line;
+    $line =~ s/ //;
     $line =~ s/^>//;
-    if (exists $heads{$line}) {
-      my $name = $heads{"$line"};
-      print OUT ">$line|$name\n";
+    if (exists $good_records{"$line"}) {
+      my $name = $good_records{"$line"};
+      $handle = $keywords{"$line"};
+      print $handle ">$line|$name\n";
       $match = 1;
-     }
-    } 
-  elsif ($match) { print OUT $line; }
+    }
+    elsif ($match) { print $handle $line; }
+  }
 }
 
-print "meu trabalho é completa!\n";
-
-foreach my $file (<OUT>, <RESULTS>, <QUERY>) { close $file; }
+# CLOSE FILES
+print "meu trabalho é completo!\n";
+foreach my $file (<MEGABLAST>, <FASTA>, <KEYWORDS>) { close $file; }
