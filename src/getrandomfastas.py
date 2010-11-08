b16cf5c4dcf9c445e67d0a380924b31c44b6de4e
Austin G. Davis-Richardson <harekrishna@gmail.com>
2010-11-08 17:18:03 -0500

    this is silly but it will work

---
+++ src/getrandomfastas.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+#Gets random fastas given a probability
+
+import sys
+from fasta import *
+
+filename, probability = sys.argv[1], sys.argv[2]
+
+yes = int(probability.split('/')[0])
+no = int(probability.split('/')[1])
+yes = yes*[True]
+no = no*[False]
+
+yesno = yes + no
+import random
+
+random.seed()
+
+with open(filename) as handle:
+    records = Fasta(handle, 'fasta')
+    for record in records:
+        if random.choice(yesno):
+            print 'yes'
+            print record
+        print 'no'
