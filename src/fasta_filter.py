1d664b6f883ac51a29931cc95f0d75d12ece9bcb
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-02-10 16:37:28 -0500

    made case insensitive

--- src/fasta_filter.py
+++ src/fasta_filter.py
@@ -6,7 +6,7 @@ with open(sys.argv[1]) as handle:
     for line in handle:
         if line.startswith('>'):
             keep = False
-            if sys.argv[2] in line:
+            if sys.argv[2] in line.lower():
                 keep = True
                 print line.strip()
         else:
