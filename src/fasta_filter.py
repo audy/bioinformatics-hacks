da169244c9f8da0c38240da619b977542542b0a7
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-02-10 16:36:51 -0500

    filters fasta files

---
+++ src/fasta_filter.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env ython
+
+import sys
+
+with open(sys.argv[1]) as handle:
+    for line in handle:
+        if line.startswith('>'):
+            keep = False
+            if sys.argv[2] in line:
+                keep = True
+                print line.strip()
+        else:
+            if keep:
+                print line.strip()
