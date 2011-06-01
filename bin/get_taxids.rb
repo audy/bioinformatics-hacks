e0eaf0a82b99db2d988c8fb1a47c4b9075befd8a
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-06-01 16:50:45 -0400

    nothing. just refactorage

--- bin/get_taxids.rb
+++ bin/get_taxids.rb
@@ -8,34 +8,45 @@ require 'hpricot'
 # always tell NCBI who they're messing with
 Bio::NCBI.default_email = "harekrishna@gmail.com"
 
-ncbi = Bio::NCBI::REST.new
-file = File.new(ARGV.pop, 'r')
-records = []
-
-file.each do |line|
-  if line.include? '>'
-    id = line.split('|')[3]
-    records << id
-  end
+if ARGV.length == 0
+  $stdout.puts "usage #{__FILE__} fasta_file_with_accessions > output"
+  exit
 end
 
+# Get accession numbers
+records = []
+File.open(ARGV.pop) do |handle|
+  handle.each do |line|
+    if line[0] == ">"
+      id = line.split('|')[3]
+      records << id
+    end
+  end
+end
 records.uniq!
+puts records
+# Query NCBI
+ncbi = Bio::NCBI::REST.new
 
 while records.length > 0
-  query = records.pop(100)
+  query = records.pop 100
   retries = 5
-  begin
-    rec = ncbi.efetch(query.join(','), {"db"=>"nucleotide", "rettype"=>"gb", "retmode"=>"xml"})
-  rescue SocketError
+  begin # get record
+    rec = ncbi.efetch(query.join(','),
+      {"db" => "nucleotide", "rettype" => "gb", "retmode" => "xml"})
+  rescue SocketError # NCBI sometimes messes up
     if (retries -= 1) > 0
       retry
     else
       next
     end
   end
+  
+  # Parse XML reply
   doc = Hpricot::XML(rec)
   numb = query.pop
   
+  # Do some weird obfuscated hpricot shit to get the taxid
   doc.search('GBSeq').each do |result|
     gi = result.search('GBSeq_accession-version>')
     result.search('GBQualifier_value').each do |h|
@@ -43,6 +54,8 @@ while records.length > 0
         a = h.to_s.split(':')
         a = a[1].split('</')
         id = a[0]
+        
+        # print to STDOUT
         puts '%s => %s' % [gi, id]
       end
     end
