8a04c406ea4c869b175ed4d9f89842299741a95c
audy <harekrishna@gmail.com>
2013-11-27 14:52:02 -0500

    refreshing

--- bin/fasta_search.py
+++
@@ -1,9 +0,0 @@
-from fasta import *
-import sys
-
-with open(sys.argv[1]) as handle:
-    for record in Fasta(handle, filetype="fasta"):
-        for word in ' '.join(sys.argv[2:]).strip('\"').split(', '):
-            if word in record.header:
-                record.sequence[-1] = record.sequence[-1].strip()
-                print '%s' % record,
