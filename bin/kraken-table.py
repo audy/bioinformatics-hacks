8f0894b8a0cc5595c7c1605ab3551a16e65d0f06
Austin Davis-Richardson <harekrishna@gmail.com>
2019-07-24 15:38:58 -0700

    reformat w/ black

--- bin/kraken-table.py
+++ bin/kraken-table.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
 # # kraken-table
-# 
+#
 # Generates a table from [Kraken](https://ccb.jhu.edu/software/kraken/)
 # output(s). Like [kraken-biom](https://github.com/smdabdoub/kraken-biom) but
 # doesn't require you to install biom-format, SciPy and Numpy just to generate a
 # table.
-# 
+#
 # ## Usage
-# 
+#
 # ```
 # ./kraken-table.py \
 #   --inputs \
@@ -22,18 +22,20 @@ import logging
 from csv import DictWriter
 from collections import defaultdict
 
+
 def parse_args():
-    '''
+    """
     return arguments
     >>> args = parse_args()
 
-    '''
+    """
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--log', default='/dev/stderr',
-                        help='log file (default=stderr)')
-    parser.add_argument('--output', default='/dev/stdout')
-    parser.add_argument('--inputs', nargs='*', default=[])
+    parser.add_argument(
+        "--log", default="/dev/stderr", help="log file (default=stderr)"
+    )
+    parser.add_argument("--output", default="/dev/stdout")
+    parser.add_argument("--inputs", nargs="*", default=[])
     return parser.parse_args()
 
 
@@ -42,19 +44,18 @@ def parse_kraken_file(handle):
         line = line.strip().split("\t")
 
         yield {
-            'classified': { 'C': True, 'U': False }[line[0]],
-            'read_id': line[1],
-            'ncbi_taxid': line[2],
-            'length': int(line[3]),
-            'assignments': line[4].split()
+            "classified": {"C": True, "U": False}[line[0]],
+            "read_id": line[1],
+            "ncbi_taxid": line[2],
+            "length": int(line[3]),
+            "assignments": line[4].split(),
         }
 
 
-
 def main():
-    '''
+    """
         >>> main() # stuff happens
-    '''
+    """
 
     args = parse_args()
     logging.basicConfig(filename=args.log, level=logging.INFO)
@@ -67,23 +68,20 @@ def main():
             kraken_data = parse_kraken_file(handle)
 
             for row in kraken_data:
-                field_names.add(row['ncbi_taxid'])
-                input_otu_counts[input][row['ncbi_taxid']] += 1
+                field_names.add(row["ncbi_taxid"])
+                input_otu_counts[input][row["ncbi_taxid"]] += 1
 
-    field_names = ['input'] + sorted([ i for i in field_names ])
+    field_names = ["input"] + sorted([i for i in field_names])
 
-    with open(args.output, 'w') as handle:
-        writer = DictWriter(handle,
-                            fieldnames=field_names)
+    with open(args.output, "w") as handle:
+        writer = DictWriter(handle, fieldnames=field_names)
 
         writer.writeheader()
 
         for input, otu_counts in list(input_otu_counts.items()):
-            otu_counts['input'] = input
+            otu_counts["input"] = input
             writer.writerow(otu_counts)
 
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
